7b197d8f3dd87a90c215cf29fecf39deae1ffe8c
Evgenii Bykov <100200240+evbkv@users.noreply.github.com>
2025-12-13 17:07:25 +0300

    Update collector.bat

--- collector.bat
+++ collector.bat
@@ -8,13 +8,44 @@ set first=1
 
 for /r %%f in (*) do (
     set "skipfile=0"
+    set "skipcontent=0"
     
     echo "%%f" | findstr /i "\\.git\\" >nul
     if not errorlevel 1 set skipfile=1
     
     if /i "%%~nxf"=="LICENSE" set skipfile=1
-    if /i "%%~nxf"=="collect_files.bat" set skipfile=1
-    if /i "%%~xf"==".sqlite" set skipfile=1
+    if /i "%%~nxf"=="collector.bat" set skipfile=1
+    
+    if !skipfile!==0 (
+        if /i "%%~xf"==".sqlite" set skipcontent=1
+        if /i "%%~xf"==".db" set skipcontent=1
+        if /i "%%~xf"==".db3" set skipcontent=1
+        if /i "%%~xf"==".sqlite3" set skipcontent=1
+        
+        if /i "%%~xf"==".ico" set skipcontent=1
+        
+        if /i "%%~xf"==".jpg" set skipcontent=1
+        if /i "%%~xf"==".jpeg" set skipcontent=1
+        if /i "%%~xf"==".png" set skipcontent=1
+        if /i "%%~xf"==".gif" set skipcontent=1
+        if /i "%%~xf"==".bmp" set skipcontent=1
+        if /i "%%~xf"==".tiff" set skipcontent=1
+        if /i "%%~xf"==".tif" set skipcontent=1
+        if /i "%%~xf"==".webp" set skipcontent=1
+        if /i "%%~xf"==".psd" set skipcontent=1
+        
+        if /i "%%~xf"==".svg" set skipcontent=1
+        if /i "%%~xf"==".ai" set skipcontent=1
+        if /i "%%~xf"==".eps" set skipcontent=1
+        if /i "%%~xf"==".pdf" set skipcontent=1
+        
+        if /i "%%~xf"==".ttf" set skipcontent=1
+        if /i "%%~xf"==".otf" set skipcontent=1
+        if /i "%%~xf"==".woff" set skipcontent=1
+        if /i "%%~xf"==".woff2" set skipcontent=1
+        if /i "%%~xf"==".eot" set skipcontent=1
+        if /i "%%~xf"==".fon" set skipcontent=1
+    )
     
     if !skipfile!==0 (
         if not exist "%%f\" (
@@ -42,7 +73,10 @@ for /r %%f in (*) do (
             )
             
             echo. >> "%output%"
-            type "%%f" >> "%output%" 2>nul
+            
+            if !skipcontent!==0 (
+                type "%%f" >> "%output%" 2>nul
+            )
         )
     )
 )
